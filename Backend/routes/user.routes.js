0cb8dfd4b5ffd290054a4a6e271ea5e4a3c9e0da
JamesRoul <mr.j.roulleau@gmail.com>
2023-07-12 12:38:08 +0200

    back

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -4,5 +4,6 @@ const userCtrl = require('../controllers/users.controller');
 
 router.post('/login', userCtrl.login);
 //router.post('/signup', userCtrl.signup);
+router.post('/signup', userCtrl.signup);
 
 module.exports = router;
